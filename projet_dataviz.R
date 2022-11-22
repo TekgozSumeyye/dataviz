f77b7ffa28bb488031d1e3d7254fb60d61b17d4c
Sumeyye Tekgoz <tekgozsumeyye69@gmail.com>
2022-11-23 00:29:13 +0100

    Ajout partie sumeyye (Mjob, Fjob et address) + quelques modifications

--- projet_dataviz.R
+++ projet_dataviz.R
@@ -1,18 +1,21 @@
 library(readr)
 library(ggplot2)
 library(labelled)
+library(dplyr)
+install.packages("RColorBrewer")
+library(RColorBrewer)
 
 maths <- read_csv("Maths.csv")
 Portuguese <- read_csv("Portuguese.csv")
 
-    ####Introduction####
+####Introduction####
 
-#Ces donnÃ©es portent sur les rÃ©sultats des Ã©lÃ¨ves dans l'enseignement 
-#secondaire de deux Ã©coles portugaises. Les attributs des donnÃ©es 
-#comprennent les notes des Ã©lÃ¨ves, les caractÃ©ristiques dÃ©mographiques, 
-#sociales et scolaires, et ont Ã©tÃ© collectÃ©s Ã  l'aide de rapports et de 
-#questionnaires scolaires. Deux ensembles de donnÃ©es sont fournis concernant
-#les performances dans deux matiÃ¨res distinctes : Les mathÃ©matiques (mat) et
+#Ces données portent sur les résultats des élèves dans l'enseignement 
+#secondaire de deux écoles portugaises. Les attributs des données 
+#comprennent les notes des élèves, les caractéristiques démographiques, 
+#sociales et scolaires, et ont été collectés à l'aide de rapports et de 
+#questionnaires scolaires. Deux ensembles de données sont fournis concernant
+#les performances dans deux matières distinctes : Les mathématiques (mat) et
 #la langue portugaise (por). 
 
 N1 <- nrow(maths)
@@ -23,42 +26,44 @@ N2 <- nrow(Portuguese)
 fulldt<-bind_rows(maths, Portuguese)
 nrow(fulldt)
 str(fulldt)
-#
+#Avec str, on visualiser les premieres donnees des variables, et on regarde si ce sont des variables qualitative ou quantitatives
 
-    #### I. Visualisation des donnees####
+#Je pense qu'il est plus interessant de creer un bdd avec uniquement les variables qu'on avait choisit 
+data<- fulldt[, c(1:4, 9:10, 22, 24:25, 29:30)]
+data_notes<-fulldt[, c(31:33)]
 
+#### I. Visualisation des donnees####
+summary(maths)
+summary(Portuguese)
 #FAIRE PRESENTATION GENERALE DES VARIABLES AVEC GRAPH
 
-    # a) Ecole
+# a) Ecole
 aggregate (x = maths$school,
            by = list(maths$school),
            FUN = length
 )
+#Pour fulldt, il y a 772 eleves scolarises a Gabriel Pereira et 272  a Mousinho da Silveira.
+barplot(table(fulldt$school), beside=TRUE,legend.text = TRUE,col=brewer.pal(n = 2, name = "Set1"),ylim=c(0,800),main="Repartition de l'ecole",xlab="ecole",ylab="Effectifs")
 
-    #b) sexe
+
+#b) sexe
 maths_sex_n <- aggregate (x = maths$sex,
-           by = list(maths$sex),
-           FUN = length
+                          by = list(maths$sex),
+                          FUN = length
 )
+barplot(table(fulldt$sex), beside=TRUE,legend.text = TRUE,col=brewer.pal(n = 2, name = "Set1"),ylim=c(0,600),main="Repartition dU sexe pour les 2 écoles",xlab="Sexe",ylab="Effectifs")
+#Il y a 591 filles et 453 garcons au sein des 2 écoles
+#Graphique sexe a GP
+table(fulldt$sex, fulldt$school)
+barplot(table(fulldt$sex, fulldt$school), beside=TRUE,legend.text = TRUE,col=brewer.pal(n = 4, name = "Set1"),ylim=c(0,600),main="Repartition dU sexe pour les 2 écoles",xlab="Sexe",ylab="Effectifs")
 
-
-summary(maths)
-summary(Portuguese)
-#Faire description des variables 
+ggplot(fulldt, aes(x = sex, y = school))+
+  geom_col(aes(fill = school), width = 0.7)
+#Demander comment faire  geom_text(aes(y = school, label = sex, group =sex), color = "white")
 
 table(maths$sex)
-
-#Savoir nombre de rÃ©sultat par variable, ex sexe =2 (h et f)
-#...
-
 ggplot(data=maths, aes(x=sex)) + geom_bar()
 
-
-table(maths$school)
-table(Portuguese$school)
-
-#PARTIE VISUALISATION
-#barre, barplot, plot
 
 table(fulldt$sex, fulldt$age)
 ggplot(fulldt, aes(fulldt$age)) + 
@@ -152,10 +157,11 @@ dotchart (sort(table(fulldt$school)))
 
 
 #Partie sumeyye
+#ADAPTER LES COULEURS
 #Table Adresse
 table(fulldt$address)
 table(maths$address)
-table(portuguese$address)
+table(Portuguese$address)
 
 
 as.data.frame(table(fulldt$address))
@@ -163,8 +169,37 @@ ggplot(as.data.frame(table(fulldt$address))) +
   geom_bar(aes(x = Var1, y = Freq), 
            stat = 'identity', fill = 'darkgreen') +
   ggtitle("Basic Bar Plot") +
-  xlab("Nombre de personne") +
-  ylab("COUNT") +
+  xlab("Adresse") +
+  ylab("Effectifs") +
   theme_bw() +
   theme(axis.text.x = element_text(face = 'bold', size = 10),
         axis.text.y = element_text(face = 'bold', size = 10))
+
+#Emploi père
+#Table Adresse
+
+
+as.data.frame(table(fulldt$Fjob))
+ggplot(as.data.frame(table(fulldt$Fjob))) +
+  geom_bar(aes(x = Var1, y = Freq), 
+           stat = 'identity', fill = 'darkgreen') +
+  ggtitle("Basic Bar Plot") +
+  xlab("Job du pere") +
+  ylab("Effectifs") +
+  theme_bw() +
+  theme(axis.text.x = element_text(face = 'bold', size = 10),
+        axis.text.y = element_text(face = 'bold', size = 10))
+
+#Emploi de la mere
+#Table Adresse
+
+
+as.data.frame(table(fulldt$Mjob))
+ggplot(as.data.frame(table(fulldt$Mjob))) +
+  geom_bar(aes(x = Var1, y = Freq), 
+           stat = 'identity', fill = 'darkgreen') +
+  ggtitle("Basic Bar Plot") +
+  xlab("Job de la mere") +
+  ylab("Effectifs") +
+  theme_bw() +
+  theme(axis.text.x = element_text(face = 'bold', size = 10),
